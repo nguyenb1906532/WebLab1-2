45aee3122c6cb2aecf407c11f183998d532d90e1
churelung123 <114229441+churelung123@users.noreply.github.com>
2022-11-04 14:15:31 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require(".app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
-app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not fonud"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Wellcome to contact book application."});
